dd639a77df75b371503830865bf787adb9ed67ae
karl-zschiebsch <karl.zschiebsch@github.com>
2024-05-20 14:38:48 +0200

    Add cproto.bat

--- freeze.sh
+++ freeze.sh
@@ -1,50 +1,16 @@
 #!/bin/bash
+echo "Setting up virtual environment ..."
+python -m venv 
 
-echo "Hello from the seekers community! cx_Freeze helper 24.4.18"
+echo "Install requirements ..."
+venv/bin/pip install -r requirements.txt
+venv/bin/pip install cx_Freeze
 
-usage() {
-  echo "Usage: $0 [-i|-s|-c|-t|-h]" 1>&2
-}
+echo "Building binaries ..."
+venv/bin/cxfreeze -c run_seekers.py --target-dir dist/seekers --include-files config.ini
+venv/bin/cxfreeze -c run_client.py --target-dir dist/client
 
-install_requirements() {
-  echo "Install requirements ..."
-  python -m venv venv
-  venv/bin/pip install -r requirements.txt
-  venv/bin/pip install cx_Freeze
-}
-
-build_seekers() {
-  echo "Building seekers ..."
-  venv/bin/cxfreeze -c run_seekers.py --target-dir dist/seekers --include-files config.ini
-}
-
-build_client() {
-  echo "Building client ..."
-  venv/bin/cxfreeze -c run_client.py --target-dir dist/client
-}
-
-test() {
-  echo "Testing build ..."
-  dist/run_seekers examples/ai-decide.py examples/ai-simple.py
-}
-
-[ $# -eq 0 ] && usage
-while getopts ":isct" arg; do
-    case $arg in
-        i)
-            install_requirements
-            ;;
-        s)
-            build_seekers
-            ;;
-        c)
-            build_client
-            ;;
-        t)
-            test
-            ;;
-        *)
-            usage
-            ;;
-    esac
-done
+echo "Compress artifacts ..."
+zip client-linux.zip dist/client
+zip seekers-linux.zip dist/seekers
+echo "Finished!"
